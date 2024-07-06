d1836635cdd50d3e6ecd2e7a08c4832946487c22
Tensor <narutokelechi@gmail.com>
2024-07-06 22:46:50 +0100

    add toml and setup.py files for library configs

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ripple_net",
+    version="0.1.0",
+    author="Kelechi[tensor]",
+    author_email="kelechichibuzo@gmail.com",
+    description="Text-image search and image tagging library",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
