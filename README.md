abf6aa636cd5ca19d19fd43bf4e586dc966ce11e
Tensor <narutokelechi@gmail.com>
2024-07-07 18:45:37 +0100

    added configs to setup.py file, readme documentation

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-### ripple
-**(in development)**
+### ripple 
+**(still in development)**
 #### Search image datasets with words, or even label images automatically
 
-Install with `pip install ripple_net` (I name things weirdly)
+Install with `pip install ripple_net` 
+(I name things weirdly :/)
+
+#### Usage guide
+
+```python
+from ripple import ImageEmbedder, ImageSearch # import classes
+
+embedder = ImageEmbedder('huggan/wikiart', is_hfdataset=True) # load from a huggingface image dataset or load from a local image directory
+
+embedded_images = embedder.create_embeddings(device="cuda", batch_size=32) # could also use 'cpu' if CUDA-enabled GPU isn't available
+
+text_search =  ImageSearch(embedded_data, data_indexer.embed_model)
+scores, images =text_search.get_similar_images(query='painting of a river', k_images=10) # specify text query for image, and number of results to return
+text_search.image_grid(images, scores) # dislay grid of returned images
--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    description = fh.read()
+
+
 setup(
     name="ripple_net",
     version="0.1.0",
-    author="Kelechi[tensor]",
+    author="Chibuzo Kelechi",
+    py_modules=["ripple"],
     author_email="kelechichibuzo@gmail.com",
     description="Text-image search and image tagging library",
     packages=find_packages(),
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kelechi-c/ripple_net",
+    keywords=["pypi", "image search", "datasets", "CLIP", "image tagging"],
+    license="Apache 2.0",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache 2.0 License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "sentence-transformers",
+        "faiss-gpu",
+        "faiss-cpu",
+        "datasets",
+        "matplotlib",
     ],
     python_requires=">=3.6",
 )
